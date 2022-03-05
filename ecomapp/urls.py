477a67612e49461fa93b3bb79b4a4ee7b609675e
Shahzod2304 <75132684+Shahzod2304@users.noreply.github.com>
2022-03-06 00:37:24 +0500

    update project

--- ecomapp/urls.py
+++ ecomapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import ContactusView, HomeView, AboutView
+from .views import ContactusView, HomeView, AboutView, AllProductsView, ProductDetailView
 app_name = "ecomapp"
 urlpatterns = [ 
     path("",HomeView.as_view(),name="home"),
     path("about/",AboutView.as_view(),name="about"),
-    path("contactus/", ContactusView.as_view(),name="contactus"),     
+    path("contactus/", ContactusView.as_view(),name="contactus"),    
+    path('all-products/', AllProductsView.as_view(), name="allproducts"), 
+    path("product/<slug:slug>/", ProductDetailView.as_view(), name="productdetail"),
 ]
 
     
--- ecomapp/views.py
+++ ecomapp/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import *
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = "home.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['myname'] = "Shahzod_Sharofiddinov"
+        context['product_list'] = Product.objects.all().order_by("-id")
+        
+        return context
+
+class AllProductsView(TemplateView):
+    template_name = "allproducts.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['allcategories'] = Category.objects.all().order_by("id")
+        return context
+
+class ProductDetailView(TemplateView):
+    template_name = 'productdetail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = self.kwargs['slug']
+        product = Product.objects.get(slug=url_slug)
+        product.view_count += 1
+        product.save()
+        context['product'] = product
+        return context
+
+    
+
 class AboutView(TemplateView):
-    template_name = "about.html"
+    template_name = "about.html"    
 
 class ContactusView(TemplateView):
     template_name = "contactus.html"
